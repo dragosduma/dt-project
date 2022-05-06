b5a17d483e32900689f4142dbaf60fe3211cb53d
dragos <dragos.duma@gmail.com>
2022-05-06 16:37:39 +0300

    Install json server

--- library_management/src/App.vue
+++ library_management/src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <Home/>
   </nav>
-  <router-view/>
 </template>
+
+<script>
+import Home from "./views/HomeView.vue"
+export default {
+  components:{
+    Home
+  }
+}
+</script>
 
 <style>
 #app {
