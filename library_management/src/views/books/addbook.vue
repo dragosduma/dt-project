8abef4dcab7d05c9e4e6729fedf91f6f0610cfa7
Ignat Eduard Gabriel <ignat.eduard1337@gmail.com>
2022-05-07 18:40:20 +0300

    menu and views for the menu

---
+++ library_management/src/views/books/addbook.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>  
+        <h4>Add Book</h4>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:"AddBook"
+    }
+</script>
+
+<style>
+
+</style>
---
+++ library_management/src/views/books/issuebook.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>  
+        <h4>Issue Book</h4>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:"IssueBook"
+    }
+</script>
+
+<style>
+
+</style>
